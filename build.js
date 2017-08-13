87fe6e7cfea5bee427b8f59cd390f58a10d3c22e
snickell <snickell@gmail.com>
2017-08-12 21:04:15 -0700

    Build

--- build.js
+++ build.js
@@ -71,6 +71,7 @@
 	__webpack_require__(22);
 	__webpack_require__(23);
 	__webpack_require__(24);
+	__webpack_require__(25);
 
 
 /***/ }),
@@ -2000,7 +2001,7 @@
 	  schema: {
 	    color: { type: 'color', default: '#ef2d5e' },
 	    size: { default: 0.01, min: 0.001, max: 0.3 },
-	    brush: { default: 'flat' },
+	    brush: { default: 'potters-wheel' },
 	    enabled: { default: true }
 	  },
 	  init: function () {
@@ -3607,6 +3608,91 @@
 /* 19 */
 /***/ (function(module, exports) {
 
+	AFRAME.registerBrush('potters-wheel',
+	  {
+	    init: function (color, width) {
+	      this.material = new THREE.MeshStandardMaterial({
+	        color: this.data.color,
+	        roughness: 0.6,
+	        metalness: 0.2,
+	        side: THREE.FrontSide,
+	        shading: THREE.SmoothShading
+	      });
+	      this.points = [];
+	      this.initialPoint = null;
+	    },
+	    regenerateMeshFromPoints: function (color, width) {
+	      if (this.mesh) this.object3D.remove(this.mesh);
+
+	      var geometry = new THREE.LatheBufferGeometry( this.points );
+	      var material = new THREE.MeshPhongMaterial( { color: this.data.color } );
+	      material.side = THREE.DoubleSide;
+	      
+	      this.mesh = new THREE.Mesh( geometry, material );
+	      this.object3D.add( this.mesh );
+	    },
+	    addPoint: function (position, orientation, pointerPosition1, pointerPosition2, pressure, timestamp) {
+	      if (pointerPosition1 === pointerPosition2) return;
+
+	      if (!this.initialPoint) {
+	        this.initialPoint = pointerPosition1.clone();
+	      }
+
+	      var newPointXZ = new THREE.Vector2(pointerPosition2.x, pointerPosition2.z);
+	      var initialPointXZ = new THREE.Vector2(this.initialPoint.x, this.initialPoint.z);
+	      var distanceXZ = newPointXZ.distanceTo(initialPointXZ);
+
+	      var newPoint = new THREE.Vector2(distanceXZ, pointerPosition2.y - this.initialPoint.y);
+
+	      this.points.push(newPoint);
+
+	      this.regenerateMeshFromPoints();
+
+	      this.mesh.position.x = this.initialPoint.x;
+	      this.mesh.position.z = this.initialPoint.z;
+	      this.mesh.position.y = this.initialPoint.y;
+	    }
+	  },
+	  {thumbnail: 'brushes/thumb_potters_wheel.png', spacing: 0.0}
+	);
+
+
+	/*
+
+	// x1, y1
+	initialPoint.x = 0
+	initialPoint.y = 0
+
+	// x2, y2
+	pointerPosition.x
+	pointerPosition.y
+
+
+
+
+
+	var angle1=2*Math.PI/3;
+	var angle2=Math.PI/6;
+
+	// u
+	var vec = pointerPosition.clone().sub(initialPoint);
+
+	var a3=vec.length();
+	var angle3=Math.PI-angle1-angle2;
+	var a2=a3*Math.sin(angle2)/Math.sin(angle3);
+
+	var RHS1=initialPoint.x*vec.x+initialPoint.y*vec.y+a2*a3*Math.cos(angle1);
+
+	var RHS2=pointerPosition.y*vec.x-pointerPosition.x*vec.y+a2*a3*Math.sin(angle1);
+	var x3=(1/(a3*a3))*(vec.x*RHS1-vec.y*RHS2);
+	var y3=(1/(a3*a3))*(vec.y*RHS1+vec.x*RHS2);
+
+	*/
+
+/***/ }),
+/* 20 */
+/***/ (function(module, exports) {
+
 	/* globals AFRAME THREE */
 	(function () {
 	  var line = {
@@ -3907,7 +3993,7 @@
 
 
 /***/ }),
-/* 20 */
+/* 21 */
 /***/ (function(module, exports) {
 
 	/* global AFRAME THREE */
@@ -4280,7 +4366,7 @@
 
 
 /***/ }),
-/* 21 */
+/* 22 */
 /***/ (function(module, exports) {
 
 	/* globals AFRAME THREE */
@@ -4338,7 +4424,7 @@
 
 
 /***/ }),
-/* 22 */
+/* 23 */
 /***/ (function(module, exports) {
 
 	/* globals AFRAME THREE */
@@ -4372,7 +4458,7 @@
 
 
 /***/ }),
-/* 23 */
+/* 24 */
 /***/ (function(module, exports) {
 
 	/* globals AFRAME THREE */
@@ -4483,7 +4569,7 @@
 
 
 /***/ }),
-/* 24 */
+/* 25 */
 /***/ (function(module, exports) {
 
 	/* globals AFRAME THREE */
